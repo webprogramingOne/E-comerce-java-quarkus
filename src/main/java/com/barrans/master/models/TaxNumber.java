96dec060333cbc0812f40b31bfe8ed82a5ee2033
deni.herdian <denihp12@gmail.com>
2022-12-19 17:01:50 +0700

    Fixing tax number

--- src/main/java/com/barrans/master/models/TaxNumber.java
+++ src/main/java/com/barrans/master/models/TaxNumber.java
@@ -13,8 +13,12 @@ public class TaxNumber extends CommonObjectActiveAndCreatedDate implements Seria
 
 	private static final long serialVersionUID = 1L;
 
-  	public String from;
-  	public String to;
+	@Column(name = "tn_from", length = 100, nullable = false)
+  	public String tnFrom;
+  	
+	@Column(name = "tn_to", length = 100, nullable = false)
+  	public String tnTo;
+  	
   	public String fixed;
   
 	@Column(name = "company_id", length = 36, nullable = false)
